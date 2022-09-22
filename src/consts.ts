80a2653b99645cbc7b9b6a4ba46f2e33b5859da7
N1colas <agostinicolas@gmail.com>
2022-09-22 16:32:49 +0200

    etat 2

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const r = 1;
+export const x0 = 50;
+export const y0 = 50;
+export const r0 = 45;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/main.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
